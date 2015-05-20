7ebb2fcff5509a0291c87c022c14a1389491f522
Gotha91 <dawidkrypto1@onet.pl>
2015-05-20 12:52:02 +0200

    Update calculator.spec.js

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -8,11 +8,11 @@ it('should return sum of two numbers', function ()
 expect(calculator.add(1, 2)).toEqual(3);
 });
 });
-+ describe('subtract', function ()
-+ {
-+ it('should return difference between second and first number', function ()
-+ {
-+ expect(calculator.subtract(1, 2)).toEqual(1);
-+ });
-+ });
+ describe('subtract', function ()
+ {
+ it('should return difference between second and first number', function ()
+ {
+ expect(calculator.subtract(1, 2)).toEqual(1);
+ });
+ });
 });
